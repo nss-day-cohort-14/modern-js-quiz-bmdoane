c5a4a2a0388aba6a065a6480e430bd338409a67d
Brian Doane <brianmdoane@gmail.com>
2016-07-11 12:04:23 -0500

    built out health and damage in constructors.  will need refactoring

--- dist/bundle.js
+++ dist/bundle.js
@@ -57,63 +57,72 @@ $(document).ready(function() {
 const Battledome = require('./robot');
 // const Type = require('./type');
 
-Battledome.Warehouse = {};
 // // Define at least 2 specific robot model functions for each type.
 // // Give each robot model a different range of health. For example, one model can have health range of 50-80, and another one will have a range of 60-120. To accomplish this, read about the Math.random() function in JavaScript.
 // // Give each robot model a different range of damage they do using the same technique.
 
-Battledome.Warehouse.ModelOne = function() {
-	this.healthBonus = 1;
-  this.damageBonus = 1;
+Battledome.ModelOne = function() {
+	this.modelName = "One";
+	this.image = "https://c7.staticflickr.com/8/7386/28210427046_feb79c8c62.jpg";
+	this.healthBonus = 20;
+  this.damageBonus = 0;
   this.shieldBonus = 1;
   this.evadeBonus = 1;
 };
-Battledome.Warehouse.ModelOne.prototype = new Battledome.TypeOne();
+Battledome.ModelOne.prototype = new Battledome.TypeOne();
 console.log("Battledome.Warehouse", Battledome.Warehouse);
 
-Battledome.Warehouse.ModelTwo = function() {
-	this.healthBonus = 2;
+Battledome.ModelTwo = function() {
+	this.modelName = "Two";
+	this.image = "https://c7.staticflickr.com/9/8806/28210427006_c32c96429a.jpg";
+	this.healthBonus = 15;
   this.damageBonus = 2;
   this.shieldBonus = 2;
   this.evadeBonus = 2;
 };
-Battledome.Warehouse.ModelTwo.prototype = new Battledome.TypeOne();
+Battledome.ModelTwo.prototype = new Battledome.TypeOne();
 
-Battledome.Warehouse.ModelThree = function() {
-	this.healthBonus = 3;
-  this.damageBonus = 3;
+Battledome.ModelThree = function() {
+	this.modelName = "Three";
+	this.image = "https://c3.staticflickr.com/9/8579/28140200322_bdc5782582.jpg";
+	this.healthBonus = 15;
+  this.damageBonus = 4;
   this.shieldBonus = 3;
   this.evadeBonus = 3;
 };
-Battledome.Warehouse.ModelThree.prototype = new Battledome.TypeTwo();
+Battledome.ModelThree.prototype = new Battledome.TypeTwo();
 
-Battledome.Warehouse.ModelFour = function() {
-	this.healthBonus = 4;
-  this.damageBonus = 4;
+Battledome.ModelFour = function() {
+	this.modelName = "Four";
+	this.image = "https://c1.staticflickr.com/9/8682/28210426896_d36d679361.jpg";	
+	this.healthBonus = 10;
+  this.damageBonus = 6;
   this.shieldBonus = 4;
   this.evadeBonus = 4;
 };
-Battledome.Warehouse.ModelFour.prototype = new Battledome.TypeTwo();
+Battledome.ModelFour.prototype = new Battledome.TypeTwo();
 
-Battledome.Warehouse.ModelFive = function() {
+Battledome.ModelFive = function() {
+	this.modelName = "Five";
+	this.image = "https://c1.staticflickr.com/8/7506/28140200152_6b341998a0.jpg";	
 	this.healthBonus = 5;
-  this.damageBonus = 5;
+  this.damageBonus = 8;
   this.shieldBonus = 5;
   this.evadeBonus = 5;
 };
-Battledome.Warehouse.ModelFive.prototype = new Battledome.TypeThree();
+Battledome.ModelFive.prototype = new Battledome.TypeThree();
 
-Battledome.Warehouse.ModelSix = function() {
-	this.healthBonus = 6;
-  this.damageBonus = 6;
+Battledome.ModelSix = function() {
+	this.modelName = "Six";
+	this.image = "https://c5.staticflickr.com/8/7629/28210426796_aa9cf2f4bd.jpg";	
+	this.healthBonus = 0;
+  this.damageBonus = 10;
   this.shieldBonus = 6;
   this.evadeBonus = 6;
 };
-Battledome.Warehouse.ModelSix.prototype = new Battledome.TypeThree();
+Battledome.ModelSix.prototype = new Battledome.TypeThree();
 
-console.log("Battledome.Warehouse", Battledome.Warehouse);
-
-let player1 = new Battledome.Warehouse.ModelFive;
+let player1 = new Battledome.ModelSix();
 console.log("player1", player1);
 
 module.exports = Battledome;
@@ -128,7 +137,7 @@ let Battledome = {};
 Battledome.Robot = function(name) {
 	this.playerName = name || "unknown robot";
 	this.type = null;
-  this.health = Math.floor(Math.random() * 40 + 50);
+  this.health = 0;
   this.damage = 0;
   this.shield = 0;
   this.evade = 0;
@@ -142,18 +151,25 @@ module.exports = Battledome;
 const Battledome = require('./robot');
 
 // Need three type constructor functions
+// Game balancing - Building damage less to more with types
 Battledome.TypeOne = function() {
 	this.type = "type one";
+	this.health = Math.floor(Math.random() * 40 + 70);
+	this.damage = Math.floor(Math.random() * 10 + 5);
 };
 Battledome.TypeOne.prototype = new Battledome.Robot();
 
 Battledome.TypeTwo = function() {
 	this.type = "type two";
+	this.health = Math.floor(Math.random() * 40 + 60);
+	this.damage = Math.floor(Math.random() * 10 + 10);
 };
 Battledome.TypeTwo.prototype = new Battledome.Robot();
 
 Battledome.TypeThree = function() {
 	this.type = "type three";
+	this.health = Math.floor(Math.random() * 40 + 50);
+	this.damage = Math.floor(Math.random() * 10 + 15);
 };
 Battledome.TypeThree.prototype = new Battledome.Robot();
 
--- js/type.js
+++ js/type.js
@@ -3,18 +3,25 @@
 const Battledome = require('./robot');
 
 // Need three type constructor functions
+// Game balancing - Building damage less to more with types
 Battledome.TypeOne = function() {
 	this.type = "type one";
+	this.health = Math.floor(Math.random() * 40 + 70);
+	this.damage = Math.floor(Math.random() * 10 + 5);
 };
 Battledome.TypeOne.prototype = new Battledome.Robot();
 
 Battledome.TypeTwo = function() {
 	this.type = "type two";
+	this.health = Math.floor(Math.random() * 40 + 60);
+	this.damage = Math.floor(Math.random() * 10 + 10);
 };
 Battledome.TypeTwo.prototype = new Battledome.Robot();
 
 Battledome.TypeThree = function() {
 	this.type = "type three";
+	this.health = Math.floor(Math.random() * 40 + 50);
+	this.damage = Math.floor(Math.random() * 10 + 15);
 };
 Battledome.TypeThree.prototype = new Battledome.Robot();
 
